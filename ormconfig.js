c3dc448bccc6387fc3943a111c762277a2734704
Gabriel Neves <gabriel-silva_2011@hotmail.com>
2020-11-04 19:43:56 -0300

    editando ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -11,8 +11,5 @@ module.exports = {
   entities: [
     `${__dirname}/${entitiesDir}/**/*.entity.${entitiesExtension}`
   ],
-  url: process.env.DATABASE_URL,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME
+  url: process.env.DATABASE_URL
 }
