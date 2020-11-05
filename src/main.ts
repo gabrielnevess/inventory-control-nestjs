a1f523e39f6e26718ca110179741dab9d87b50c9
Gabriel Neves <gabriel-silva_2011@hotmail.com>
2020-11-04 22:21:55 -0300

    adicionando cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ env.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const port = process.env.PORT || 8080;
   await app.listen(port);
 }
